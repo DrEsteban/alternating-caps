6571965d4fc007e789535a1ba03f44f254b8bc63
Steven Ryland <stevenryland@microsoft.com>
2024-08-24 17:26:51 -0700

    asdf

--- wwwroot/script.ts
+++ wwwroot/script.ts
@@ -1,11 +1,36 @@
-function isWhitespace(char: string): boolean {
-  return /\s/.test(char);
-}
+var capitalCheckbox: HTMLInputElement;
+var randomnessCheckbox: HTMLInputElement;
+var regenerateButton: HTMLButtonElement;
+var randomOptionsDiv: HTMLDivElement;
+var inputTextBox: HTMLInputElement;
+
+document.addEventListener('DOMContentLoaded', () => {
+  capitalCheckbox = document.getElementById('capitalCheckbox') as HTMLInputElement;
+  randomnessCheckbox = document.getElementById('randomnessCheckbox') as HTMLInputElement;
+  regenerateButton = document.getElementById('regenerateButton') as HTMLButtonElement;
+  randomOptionsDiv = document.getElementById('randomOptions') as HTMLDivElement;
+  inputTextBox = document.getElementById('textInput') as HTMLInputElement;
+});
 
 function alternateCaps(): void {
-  let startsWithCapital = (document.getElementById('capitalCheckbox') as HTMLInputElement).checked;
-  const introduceRandomness = (document.getElementById('randomnessCheckbox') as HTMLInputElement).checked;
-  const input = (document.getElementById('textInput') as HTMLInputElement).value;
+  if (capitalCheckbox.checked) {
+    capitalCheckbox.disabled = false;
+    randomnessCheckbox.checked = false;
+    randomnessCheckbox.disabled = true;
+  } else if (randomnessCheckbox.checked) {
+    randomnessCheckbox.disabled = false
+    capitalCheckbox.checked = false;
+    capitalCheckbox.disabled = true;
+  } else {
+    capitalCheckbox.disabled = false;
+    randomnessCheckbox.disabled = false;
+  }
+
+  const introduceRandomness = randomnessCheckbox.checked;
+  regenerateButton.hidden = !introduceRandomness;
+  randomOptionsDiv.hidden = !introduceRandomness;
+  let startWithCapital = capitalCheckbox.checked;
+  const input = inputTextBox.value;
   let result = '';
   let j = 0;
   for (let i = 0; i < input.length; i++) {
@@ -13,10 +38,13 @@ function alternateCaps(): void {
     if (isWhitespace(c)) {
       result += c;
     } else {
-      if (introduceRandomness && Math.random() < 0.1) {
-        startsWithCapital = !startsWithCapital;
+      if (introduceRandomness) {
+        const randomnessRatio = parseInt((document.getElementById('randomRatio') as HTMLInputElement).value) / 100;
+        if (Math.random() < randomnessRatio) {
+          startWithCapital = !startWithCapital;
+        }
       }
-      if (startsWithCapital) {
+      if (startWithCapital) {
         result += j % 2 === 0 ? c.toUpperCase() : c.toLowerCase();
       } else {
         result += j % 2 === 0 ? c.toLowerCase() : c.toUpperCase();
@@ -25,6 +53,10 @@ function alternateCaps(): void {
     }
   }
   (document.getElementById('textOutput') as HTMLInputElement).value = result;
+}
+
+function isWhitespace(char: string): boolean {
+  return /\s/.test(char);
 }
 
 async function copyToClipboard(): Promise<void> {
